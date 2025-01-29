492e346a9ef22ca5fd6aaaef4e6669bb2b409e9e
Guilherme Leobas <guilhermeleobas@gmail.com>
2025-01-29 12:38:33 -0300

    Add `sys.exc_info` and `sys.exception`

--- torch/_dynamo/variables/builder.py
+++ torch/_dynamo/variables/builder.py
@@ -14,6 +14,8 @@ import math
 import operator
 import random
 import re
+import sys
+import traceback
 import types
 import warnings
 import weakref
@@ -143,6 +145,8 @@ from .functions import (
     CollectiveFunctionRewriteVariable,
     CreateTMADescriptorVariable,
     FunctoolsPartialVariable,
+    SysFunctionVariable,
+    TracebackVariable,
     TritonKernelVariable,
     UserFunctionVariable,
     UserMethodVariable,
@@ -1013,6 +1017,10 @@ class VariableBuilder:
         elif is_lru_cache_wrapped_function(value):
             self.install_guards(GuardBuilder.TYPE_MATCH)
             return WrapperUserFunctionVariable(value, "__wrapped__", source=self.source)
+        elif value in (traceback.clear_frames,):
+            return TracebackVariable(source=self.source)
+        elif value in (sys.exc_info, sys.exception):
+            return SysFunctionVariable(value, source=self.source)
         elif is_function_or_wrapper(value) and inspect.getattr_static(
             value, "_torchdynamo_inline", False
         ):
